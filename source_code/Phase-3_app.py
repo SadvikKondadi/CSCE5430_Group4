7736cdd527a4349b0a4dd073298300ac43b314a4
chandratej0510 <sadvikkondadi@my.unt.edu>
2025-04-20 16:25:42 -0500

    RAG_Embeddings

--- source_code/Phase-3_app.py
+++ source_code/Phase-3_app.py
@@ -1,1 +1,79 @@
+import streamlit as st
+from pymongo import MongoClient
+from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+import re
+from pymongo import MongoClient
+import gridfs
+import base64
+import io
+from collections import Counter
+from collections import defaultdict
+import pandas as pd
+import numpy as np
+import openai
+
+# Simulated user database
+client = MongoClient('mongodb+srv://krrish852456:krrish852456@cluster0.99khz.mongodb.net/?retryWrites=true&w=majority&appid=Cluster0')
+
+db = client["slms"]
+dba=client['assign']
+collection = db["slms"]
+collection1 = db["Subjects"]
+collection2 = db["Instructor"]
+cust = db["Customer Care"]
+assign=db["assign"]
+sum=db["Summary"]
+m=db["modules"]
+p=db["payment"]
+a=db["Attempt"]
+ass=db["Exam"]
+ab=db["Attendance"]
+ag=db["AgAttendance"]
+f=db["Feedback"]
+carddata=db["card"]
+ansd=db["Ansd"]
+fs = gridfs.GridFS(db)
+fsa = gridfs.GridFS(dba)
+# Session state initialization
+if "reg_in" not in st.session_state:
+    st.session_state["reg_in"] = False
+if "logged_in" not in st.session_state:
+    st.session_state["logged_in"] = False
+    st.session_state["userid"] = ""
+    st.session_state["role"] = ""
+if "messages" not in st.session_state:
+    st.session_state.messages = {}  # Store chat history per user
+if "admin_joined" not in st.session_state:
+    st.session_state.admin_joined = {}
+if 'mar' not in st.session_state:
+    st.session_state.mar=0
+if 'mark' not in st.session_state:
+    st.session_state.mark=0
+if 'exam' not in st.session_state:
+    st.session_state.exam=0
+if 'descriptive' not in st.session_state:
+    st.session_state.descriptive=0
+if 'call' not in st.session_state:
+    st.session_state.call=0
+if "ms" not in st.session_state:
+    st.session_state["ms"] = [{"role": "assistant", "content": "Enter your profession to get course recommendations:"}]
+
+#Logout Function
+def logout():
+    st.session_state["logged_in"] = False
+    st.session_state["userid"] = ""
+    st.session_state["role"] = ""
+    st.session_state["rerun"] = True
+    st.rerun()
+
+openai.api_key = st.secrets["OPEN_API_KEY"]
+
+def get_embedding(text, model="text-embedding-3-small"):
+    response = openai.embeddings.create(
+        input=text,
+        model=model
+    )
+    return response.data[0].embedding
 
